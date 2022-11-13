14f5753de3d79cc57fc5a1c8654df3db51604156
kkkostyaa <k.konstantin1988@gmail.com>
2022-11-13 14:12:53 +0200

    toaster

--- cypress/e2e/hw_toaster.cy.js
+++ cypress/e2e/hw_toaster.cy.js
@@ -4,7 +4,7 @@
 
 let toasterConfig = [{
     position: 'top-right',
-    title: 'Magenta toaster',
+    title: 'Blue toaster',
     content: 'that show in top-right corner',
     time: '5000',
     type: 'primary'
@@ -18,7 +18,7 @@ let toasterConfig = [{
 },
 {
     position: 'bottom-left',
-    title: 'Blue toaster',
+    title: 'Light-blue toaster',
     content: 'that show in bottom-left corner',
     time: '5000',
     type: 'info'
@@ -33,43 +33,43 @@ let toasterConfig = [{
 ];
 
 let toasterExpect =  [{
-      icon: '',
-      title: 'Magenta toaster',
+      icon: 'email',
+      title: 'Blue toaster',
       content: 'that show in top-right corner',
-      color: '',
+      color: 'rgb(98, 0, 238)',
       position: {
-          posX: 0,
-          posY: 0, 
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',  
       }
 },
 {
-      icon: '',
+      icon: 'checkmark',
       title: 'Green toaster',
       content: 'that show in top-left corner',
-      color: '',
+      color: 'rgb(96, 175, 32)',
       position: {
-          posX: 0,
-          posY: 0, 
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',  
       }
     },
 {
-      icon: '',
-      title: 'Blue toaster',
+      icon: 'question-mark',
+      title: 'Light-blue toaster',
       content: 'that show in bottom-left corner',
-      color: '',
+      color: 'rgb(4, 149, 238)',
       position: {
-          posX: 0,
-          posY: 0, 
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start', 
       }
     },
 {
-      icon: '',
+      icon: 'alert-triangle',
       title: 'Orange toaster',
       content: 'that show in bottom-right corner',
-      color: '',
+      color: 'rgb(255, 159, 5)',
       position: {
-          posX: 0,
-          posY: 0, 
+        justifyContent: 'flex-end',
+        alignItems: 'flex-start',
       }
 }
 ]
@@ -89,33 +89,54 @@ function generateToast(randToastConfig) {
 
     cy.get('nb-card-footer > .status-basic').click();
 
+}
+
+function checkToast(toastExpected) {
+    cy.get('nb-toast[ng-reflect-toast]').then(toast => {
+        cy.wrap(toast).should('contain', toastExpected.title)
+            .and('contain', toastExpected.content)
+            .and('have.css', 'background-color')
+            .and('eq', toastExpected.color);
+        
+        cy.wrap(toast).find(`g[data-name="${toastExpected.icon}"]`).should('exist');
+
+        cy.get('div[dir="ltr"]').should('have.css', 'justify-content').and('eq', toastExpected.position.justifyContent);
+        cy.get('div[dir="ltr"]').should('have.css', 'align-items').and('eq', toastExpected.position.alignItems);
+        
+    })
+
 
 }
 
 
 
-it("toaster HW", () => {
+before('Open page', () => {
     cy.visit('https://sanitarskyi-ngx-admin.herokuapp.com/');
-    cy.get('nb-card-header').contains('Material Dark').click();
+    cy.get('nb-card-header').contains('Material Light').click();
     cy.get('.ng-tns-c141-19 > .menu-title').click();
     cy.get('.menu-item.ng-tns-c141-23 > .ng-star-inserted').click();
+    cy.get('[ng-reflect-icon="menu-outline"]').click();
     cy.wait(2000);
-
-    for (let i = 0; i < 1; i++) {
-         generateToast(toasterConfig[i]);
+});
 
 
-        if (cy.get('.ng-tns-c209-54').children().should('exist')) { cy.log('exist') };
+
+it("toaster HW", () => {
+   
+
+    for (let i = 0; i < 4; i++) {
+        generateToast(toasterConfig[i]);
+        
+        checkToast(toasterExpect[i]);
+
+
+        //if (cy.get('.ng-tns-c209-54').children().should('exist')) { cy.log('exist') };
         
         
         
         //cy.log(cy.get('.ng-tns-c209-55').should('exist'));
         
-        
-        
-        
         //if (cy.get('.ng-tns-c209-54').find('.ng-reflect-toast').lenght > 0) { cy.log('exist') };
-
 
         //if (cy.get('.ng-tns-c209-55').children().should('exist')) {cy.log('not exist')}
         //.cdk-overlay-container > :nth-child(1) > :nth-child(1) > :nth-child(1) > :nth-child(1)
@@ -132,36 +153,23 @@ it("toaster HW", () => {
         //color
         //.cdk-overlay-container > :nth-child(1) > :nth-child(1) > :nth-child(1)  > :nth-child(1) 
         
+        /*
+        cy.get('body').then(($body) => {
+        if($body.find('nb-toastr-container')) {
+            cy.get('nb-toastr-container').then(toaster => {
+                cy.wrap(toaster).should('contain', expected.title)
+                .and('contain', expected.content)
+                .and('have.css', 'background color')
+                .and('eq', expected.color)
+                
+            })
         
-        
+        */
         
         
         cy.wait(7000);
     }
 
-
-
-
-/*
-    cy.get('.row > :nth-child(1) > :nth-child(1) > .mat-ripple > .select-button').click();
-    cy.get('[ng-reflect-value="bottom-left"]').click();
-
-    cy.get(':nth-child(2) > .form-group > .mat-ripple > .select-button').click();
-    cy.get('[ng-reflect-value="success"]').click();
-    
-    cy.get('.row > :nth-child(1) > :nth-child(2) > .size-medium').clear().type('locator detected');
-    cy.get('form.ng-untouched > .form-group > .size-medium').clear().type('locator detected');
-    cy.get('.row > :nth-child(1) > :nth-child(4) > .size-medium').clear().type('5000');
-
-    cy.get('nb-card-footer > .status-basic').click();
-
-    function get_rand(max_value) {
-        return Cypress._.random(0, max_value)
-    } 
-
-    cy.log(get_rand(4));
-    cy.log(get_rand(3));
-   */
 
 
 })
